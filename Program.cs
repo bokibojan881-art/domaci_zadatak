cd2ed6b1092a98c66bb65061361ff15f18704ecf
bokibojan881-art <bokibojan881@gmail.com>
2025-12-01 17:15:44 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
+## Решење
 
+``` cs
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Програм за израчунавање полупречника описаног круга правоугаоника.");
+        Console.Write("Унесите дужину дијагонале d: ");
+
+        string input = Console.ReadLine();
+
+        if (!double.TryParse(input, out double d) || d <= 0)
+        {
+            Console.WriteLine("Грешка: потребно је унети позитиван број.");
+            return;
+        }
+
+        double R = d / 2.0;
+
+        Console.WriteLine($"Полупречник описаног круга R = {R}");
+    }
+}
+```
